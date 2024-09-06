b1bf7f5140cbde24864ae512af17c14b00364b30
Mohit Harsh <mohitharshxiib17@gmail.com>
2024-09-06 15:02:21 +0530

    modified frontend

--- JobPosts/src/main/java/com/spring/JobPosts/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.JobPosts.repository;
-
-import com.spring.JobPosts.model.JobPosts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<JobPosts,Integer> {
-}
