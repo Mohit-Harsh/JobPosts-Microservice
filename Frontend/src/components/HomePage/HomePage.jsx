ce89c64d6bcc17bec63b3777a4267c19a88ad9d0
Mohit Harsh <mohitharshxiib17@gmail.com>
2024-09-06 18:30:43 +0530

    modified urls

--- Frontend/src/components/HomePage/HomePage.jsx
+++ Frontend/src/components/HomePage/HomePage.jsx
@@ -8,17 +8,13 @@ import { Link } from "react-router-dom";
 
 export default function HomePage()
 {
-    let posts = [{"date":"20 May, 2024","company":"Microsoft","title":"DevOps Engineer","skills":["Linux","Docker","Kubernetes","Azure Cloud"],"salary":"Rs. 25 LPA","location":"Hyderabad, India","type":"On-site","description":"Software Engineer responsibilities include gathering user requirements, defining system functionality and writing code in various languages, like Java, Ruby on Rails or .NET programming languages (e.g. C++ or JScript.NET.) Our ideal candidates are familiar with the software development life cycle (SDLC) from preliminary system analysis to tests and deployment."},
-                   {"date":"23 May, 2024","company":"Amazon","title":"Amazon SDE - II","skills":["DSA","Problem Solving","Coding","AWS Solution Architect"],"salary":"Rs. 30 LPA","location":"Hyderabad, India","type":"Hybrid","description":"Software Engineer responsibilities include gathering user requirements, defining system functionality and writing code in various languages, like Java, Ruby on Rails or .NET programming languages (e.g. C++ or JScript.NET.) Our ideal candidates are familiar with the software development life cycle (SDLC) from preliminary system analysis to tests and deployment."},
-                   {"date":"18 May, 2024","company":"Jp Morgan","title":"Software Engineer II","skills":["Microservices","DevOps","Problem Solving","Cloud Computing"],"salary":"Rs. 22.5 LPA","location":"Hyderabad, India","type":"Remote","description":"Software Engineer responsibilities include gathering user requirements, defining system functionality and writing code in various languages, like Java, Ruby on Rails or .NET programming languages (e.g. C++ or JScript.NET.) Our ideal candidates are familiar with the software development life cycle (SDLC) from preliminary system analysis to tests and deployment."},
-                   {"date":"21 May, 2024","company":"Amazon","title":"Amazon SDE I","skills":["Microservices","Problem Solving","AWS Solution Architect"],"salary":"Rs. 17.5 LPA","location":"Hyderabad, India","type":"Hybrid","description":"Software Engineer responsibilities include gathering user requirements, defining system functionality and writing code in various languages, like Java, Ruby on Rails or .NET programming languages (e.g. C++ or JScript.NET.) Our ideal candidates are familiar with the software development life cycle (SDLC) from preliminary system analysis to tests and deployment."}]
-                  
+    
     const [jobposts,setPosts] = useState([]);
 
 
     useEffect(() => {
         async function fetchdata(){
-            const data = (await axios.get("http://localhost:8080/post/")).data;
+            const data = (await axios.get("http://jobposts:8080/post/")).data;
             setPosts(data);
             console.log(data);
         }
@@ -31,11 +27,11 @@ export default function HomePage()
         let response = null
         if(text.length == 0)
         {
-            response = await axios.get("http://localhost:8080/post/");
+            response = await axios.get("http://jobposts:8080/post/");
         }
         else
         {
-            response = await axios.get(`http://localhost:8080/post/search/${text}`);
+            response = await axios.get(`http://jobposts:8080/post/search/${text}`);
         }
         setPosts(response.data);
     }
